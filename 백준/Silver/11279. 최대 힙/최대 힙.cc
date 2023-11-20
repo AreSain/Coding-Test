6c27580711300dbf7e636c3664c4b84a13d0ad4b
AreSain <111065574+AreSain@users.noreply.github.com>
2023-11-20 19:30:52 +0900

    [Silver II] Title: 최대 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <queue>
+#include <cstdio>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int i, n;
+	priority_queue<int, vector<int>, less<int>> pq;
+
+	cin >> n;
+	while(n > 0)
+	{
+		cin >> i;
+		if (i == 0)
+		{
+			if(pq.empty())
+				cout << "0\n";
+			else
+			{
+				cout << pq.top() << "\n";
+				pq.pop();
+			}
+		} 
+		else
+			pq.push(i);
+		n--;
+	}
+}
