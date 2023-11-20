3528d804939da1af4e4c10d5b6ce750dadbed9e4
AreSain <111065574+AreSain@users.noreply.github.com>
2023-11-20 19:29:04 +0900

    [Bronze III] Title: 열 개씩 끊어 출력하기, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/11721. 열 개씩 끊어 출력하기/열 개씩 끊어 출력하기.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+	int j = 0;
+	char arr[101];
+	scanf("%s", arr);
+	while (arr[j] != '\0')
+	{
+		printf("%c", arr[j]);
+		if (j % 10 == 9)
+		{
+			printf("\n");
+		}
+		j++;
+	}
+	return 0;
+}
