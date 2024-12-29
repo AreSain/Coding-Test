91f775a35b297dbae0520ccd914b7750c1f3f47a
seungsje <seungsje@student.42seoul.kr>
2024-12-29 23:55:06 +0900

    [Feature] 주사위 고르기

---
+++ 프로그래머스/Lv. 3/258709. JAVA 주사위 고르기/README.md
@@ -0,0 +1,3 @@
+https://school.programmers.co.kr/learn/courses/30/lessons/258709
+
+테스트 케이스에서 시간초과가 있는데, 일단 값이 틀리지는 않는 것 같아 제출하고 최적화 하려 한다.
---
+++ 프로그래머스/Lv. 3/258709. JAVA 주사위 고르기/주사위 고르기.java
@@ -0,0 +1,84 @@
+import static java.util.Arrays.*;
+import java.util.Arrays;
+class Solution {
+	public static int[] goat, playerA;
+	public static int maxWin = 0, n, diceIndex = 0;
+	public int[] solution(int[][] dice) {
+		n = dice.length;
+		goat = new int[n / 2];
+		playerA = new int[n / 2];
+		roop(playerA, dice, 0, new boolean[n]);
+
+		Arrays.sort(goat);
+		return goat;
+	}
+
+	public static void roop(int[] playerA, int[][] dice, int turn, boolean[] used) {
+		if (turn == n / 2) {
+			solve(dice, Arrays.copyOf(playerA, playerA.length));
+			return;
+		}
+		for (int i = 0; i < n; i++) {
+			if (!used[i]) {
+				used[i] = true;
+				playerA[turn] = i;
+				roop(playerA, dice, turn + 1, used);
+				used[i] = false;
+			}
+		}
+	}
+
+	public static void solve(int[][] dice, int[] playerA) {
+		int[] playerADiceResult = new int[(int)Math.pow(6, n / 2)];
+		int[] playerBDiceResult = new int[(int)Math.pow(6, n / 2)];
+		diceIndex = 0;
+		rollAllDice(dice, playerA, 0, 0, playerADiceResult);
+
+		int[] playerB = new int[n / 2];
+		boolean[] used = new boolean[n];
+		for (int i = 0; i < playerA.length; i++) {
+			used[playerA[i]] = true;
+		}
+		int idx = 0;
+		for (int i = 0; i < n; i++) {
+			if (!used[i]) {
+				playerB[idx++] = i;
+			}
+		}
+
+		diceIndex = 0;
+		rollAllDice(dice, playerB, 0, 0, playerBDiceResult);
+
+		sort(playerADiceResult);
+		sort(playerBDiceResult);
+
+		int win = 0;
+		int playerBIndex = 0;
+
+    for (int i = 0; i < playerADiceResult.length; i++) {
+        while (playerBIndex < playerBDiceResult.length && 
+               playerBDiceResult[playerBIndex] < playerADiceResult[i]) {
+            playerBIndex++;
+        }
+        win += playerBIndex;
+    }
+
+		if (win > maxWin) {
+			maxWin = win;
+			for (int i = 0; i < n / 2; i++) {
+				goat[i] = playerA[i] + 1;
+			}
+		}
+	}
+
+	public static void rollAllDice(int[][] dice, int[] player, int turn, int currentSum, int[] results) {
+		if (turn == n / 2) {
+			results[diceIndex] = currentSum;
+			diceIndex++;
+			return;
+		}
+		for (int i = 0; i < 6; i++) {
+			rollAllDice(dice, player, turn + 1, currentSum + dice[player[turn]][i], results);
+		}
+	}
+}
